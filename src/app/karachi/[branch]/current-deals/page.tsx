f714f50a8d386728666c35eb797eec395e227eec
sanasarahssalon8-cmd <sanasarahssalon8@gmail.com>
2026-01-29 18:40:08 +0500

    edge runtime edited

--- src/app/karachi/[branch]/current-deals/page.tsx
+++ src/app/karachi/[branch]/current-deals/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const runtime = 'edge';
 import React, { useMemo, useState } from "react";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
